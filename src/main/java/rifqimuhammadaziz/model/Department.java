662ca704e2760b8614ff0b48a5a28117bb0dbe6e
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-04-11 09:08:25 +0700

    add model

---
+++ src/main/java/rifqimuhammadaziz/model/Department.java
@@ -0,0 +1,15 @@
+package rifqimuhammadaziz.model;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Department {
+
+    private int id;
+    private String name;
+
+}
--- src/main/java/rifqimuhammadaziz/model/Student.java
+++ src/main/java/rifqimuhammadaziz/model/Student.java
@@ -1,4 +1,18 @@
 package rifqimuhammadaziz.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Student {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private Department department;
+
 }
