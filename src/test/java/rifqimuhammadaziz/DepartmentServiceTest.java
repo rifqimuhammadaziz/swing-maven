848dfb835b2352d12014e9171c226cb34be0878b
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-04-14 09:40:49 +0700

    department : test findById dao

--- src/test/java/rifqimuhammadaziz/DepartmentServiceTest.java
+++ src/test/java/rifqimuhammadaziz/DepartmentServiceTest.java
@@ -3,12 +3,14 @@ package rifqimuhammadaziz;
 import org.junit.Test;
 import rifqimuhammadaziz.model.Department;
 import rifqimuhammadaziz.service.DepartmentDaoImpl;
+import rifqimuhammadaziz.service.UserDaoImpl;
 
 import java.sql.SQLException;
 
 public class DepartmentServiceTest {
 
     public DepartmentDaoImpl departmentDao;
+    Department department = new Department();
 
     @Test
     public void testAddData() throws SQLException, ClassNotFoundException {
@@ -29,5 +31,13 @@ public class DepartmentServiceTest {
         for (Department department : departments) {
             System.out.println(department);
         }
+    }
+
+    @Test
+    public void testFindById() throws SQLException, ClassNotFoundException {
+        departmentDao = new DepartmentDaoImpl();
+        Integer id = 1;
+        department = departmentDao.findById(id);
+        System.out.println(department);
     }
 }
